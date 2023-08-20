45bac78de357cd28129c416863887e02682d6f6e
Islam Hany <islam.hany@rasport.com.eg>
2023-08-20 10:12:16 +0300

    ListNode of a linkedlist

---
+++ ListNode.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.leetcodeproblems;
+
+/**
+ *
+ * @author islam.hany
+ */
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) 
+      { 
+          this.val = val; 
+      this.next = next; 
+      }
+     public ListNode(ListNode another)
+      {
+          this.next=next;
+          this.val=another.val;
+      }
+  }
